ea1a9bc8892d42bfd8157aec25be93d20be7abcc
imjord <dean.jay.goat@gmail.com>
2021-07-27 20:22:58 -0600

    routes initial

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express'); // importing a library from node module
+const app = express(); // can use to chain express methods 
+const path = require('path')  // using path module to join views render 
+const PORT = process.env.port || 3001; // heroku port 
+
+
+
+app.set('views', path.join(__dirname, 'views'))   // join views dir
+
+
+app.get('/home', (req, res, next) => {
+    res.send('');  
+})
+
+// home page
+
+
+
+// 
+app.get('/resturants', (req,res, next) => {
+    const data = {
+
+    }
+
+    res.json(data)
+}
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('server live');
+})
