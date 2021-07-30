7be067ce60d9019ef6cac1b2ffb4771d80ae5745
Chris-L985 <thelopezdispenser@gmail.com>
2021-07-29 20:32:22 -0600

    added authorization to the utilities folder

---
+++ seeds/restuarant-seeds.js
@@ -0,0 +1,11 @@
+// //INSERT INTO restaurants (rest_name)
+// VALUES 
+// ('McDonalds'),
+// ('Burger King'),
+// ('Rancheritos'),
+// ('Wendys'),
+// ('Taco Bell').
+// ('Taco Time'),
+// ('Panda Express'),
+// ('Arbys'),
+// ('Carls Jr');
