d7ae8c2724be2dd962aaaab018eda26877eb92e2
Chris-L985 <thelopezdispenser@gmail.com>
2021-08-01 13:15:49 -0600

    applied minor repairs to server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 // Api routes
 const router = require('express').Router();
 
+const apiRoutes = require('../api')
 const userRoutes = require('../api/user-routes');
 const commentRoutes = require('../api/comment-routes');
 
-router.use('/users', userRoutes);
-router.use('/comments', commentRoutes);
+// router.use('/users', userRoutes);
+// router.use('/comments', commentRoutes);
 
 module.exports  = router;
