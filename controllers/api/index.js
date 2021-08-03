da216370bf3b93ae5b41ef2ba2d2082eca3b1673
imjord <dean.jay.goat@gmail.com>
2021-08-02 20:11:25 -0600

    LIVE BRO

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-
+const sequelize = require('../../config/connection');
 
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
+
 const loginRoutes = require('./login-route.js');
 
 
 router.use('/', homeRoutes);
-router.use('/login', loginRoutes);
-router.use('/api', apiRoutes);
+// router.use('/login', loginRoutes);
+// router.use('/api', apiRoutes);
 
 
 module.exports  = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+// const { restaurants , User, Likes } = require('../models');
+
+
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+
+module.exports = router;
