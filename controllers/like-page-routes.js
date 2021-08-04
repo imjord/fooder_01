3522feece090c316a278b86d6cbeb6d922fcc04f
Juan Hernandez <juanh22@gmail.com>
2021-08-04 15:11:35 -0600

    added more code for routes

--- controllers/like-page-routes.js
+++ controllers/like-page-routes.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 
+// get all likes to like page
+
 router.get('/likes-page', (req, res) => {
     res.render('likes-page');
   });
+
+
+
+  module.exports = router;
