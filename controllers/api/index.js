72d119fd3446f47af3270f86643bf621e5bdf618
imjord <dean.jay.goat@gmail.com>
2021-08-04 18:25:46 -0600

    messing with login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 
-
-const apiRoutes = require('./api/');
-
-const loginRoutes = require('./login-route.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 
-router.use('/', homeRoutes);
-// router.use('/login', loginRoutes);
-// router.use('/api', apiRoutes);
+
+router.use('/users', userRoutes);
 
 
 module.exports  = router;
